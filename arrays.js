f90d8718e686463acbabc5a99c4839ad23733f54
Josue Rios <josuerd89@gmail.com>
2018-01-17 18:37:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,15 @@ function addElementToBeginningOfArray(array, n){
   array=[n, ...array]
   return array
 }
+
+function  addElementToEndOfArray(array,n){
+  var array
+  array=[...array,n]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array,n){
+  var array
+  array.push(n)
+  return array
+}
