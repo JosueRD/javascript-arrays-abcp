4a70a8da9317a21ee9d3d3d03c12084b87f50148
Josue Rios <josuerd89@gmail.com>
2018-01-17 18:32:38 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var chocolateBars =["snickers","hundred grand", "kitkat", "skittles"]
+
+function destructivelyAddElementToBeginningOfArray(array, n){
+  var array
+  array.unshift(n)
+  return array
+}
+
+function addElementToBeginningOfArray(array, n){
+  var array
+  return array
+}
